c82b46b6f648fbd5b25a0eebf59c30a76a57d8a6
Ska Lee <ska2519@gmail.com>
2020-08-13 11:53:33 +0700

    114. Using assertions for better widget API design

---
+++ lib/app/sign_in/social_sign_in_button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/common_widgets/custom_raised_button.dart';
+
+class SocialSignInButton extends CustomRaisedButton {
+  SocialSignInButton({
+    @required String assetName,
+    @required String text,
+    Color textColor,
+    Color color,
+    VoidCallback onPressed,
+  })  : assert(assetName != null),
+        assert(text != null),
+        super(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Image.asset(assetName),
+              Text(
+                text,
+                style: TextStyle(
+                  color: textColor,
+                  fontSize: 15.0,
+                ),
+              ),
+              Opacity(
+                opacity: 0,
+                child: Image.asset(assetName),
+              ),
+            ],
+          ),
+          color: color,
+          borderRadius: 8.0,
+          onPressed: onPressed,
+        );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/app/sign_in/sign_in_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,6 +8,12 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'Time Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: SignInPage(),
+    );
   }
 }
