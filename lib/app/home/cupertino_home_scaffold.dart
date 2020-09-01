682fe29508d99576a09c629de1dec1bb0bb87d26
Ska Lee <ska2519@gmail.com>
2020-09-02 03:23:42 +0700

    Finish Section 21: Bottom Navigation with the Cupertino widgets
    358. The CupertinoPageRoute
    359. Handling the Android back button with
    WillPopScope and navigator keys
    360. Adding pop-to-root navigation
    361. Wrap up on multiple navigators
    
    ↓ Commit Message Samples ➡
    How to write Commit message
    
    Refactor subsystem X for readability
    Update getting started documentation
    Remove deprecated methods
    Release version 1.0.0
    
    Fixed bug with Y
    Changing behavior of X
    
    commit messages get written as a description of their contents:
    More fixes for broken stuff
    Sweet new API methods

--- lib/app/home/cupertino_home_scaffold.dart
+++ lib/app/home/cupertino_home_scaffold.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:time_tracker_flutter_course/app/home/jobs/jobs_page.dart';
 import 'package:time_tracker_flutter_course/app/home/tab_item.dart';
 
 class CupertinoHomeScaffold extends StatelessWidget {
-  const CupertinoHomeScaffold(
-      {Key key,
-      @required this.currentTab,
-      @required this.onSelectTab,
-      @required this.widgetBuilders})
-      : super(key: key);
+  const CupertinoHomeScaffold({
+    Key key,
+    @required this.currentTab,
+    @required this.onSelectTab,
+    @required this.widgetBuilders,
+    @required this.navigatorKeys,
+  }) : super(key: key);
 
   final TabItem currentTab;
   final ValueChanged<TabItem> onSelectTab;
